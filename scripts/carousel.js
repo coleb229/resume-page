7ee2ae39690aaf86d3a3adeface8e8ce41b0b311
Cole <coleb229@yahoo.com>
2023-08-28 18:40:49 -0700

    Fixed an issue with the carousel script breaking on mobile displays

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -2,23 +2,31 @@ const carouselSlide = document.querySelector('.carousel-slide')
 const images = document.querySelectorAll('.carousel-slide img')
 
 let counter = 0
-const slideWidth = images[0].clientWidth
+let slideWidth = images[0].clientWidth
+
+function updateSlideWidth() {
+  slideWidth = images[0].clientWidth
+}
 
 function nextSlide() {
   if (counter >= images.length - 1) {
-    counter = -1
-    carouselSlide.style.transition = 'none'
-    carouselSlide.style.transform = `translateX(-${counter * slideWidth}px)`
+    counter = 0
   } else {
     counter++
-    carouselSlide.style.transform = `translateX(-${counter * slideWidth}px)`
-    carouselSlide.style.transition = 'transform 0.4s ease-in-out'
   }
+  updateSlidePosition()
 }
 
 function prevSlide() {
-  if (counter <= 0) return
-  counter--
+  if (counter <= 0) {
+    counter = images.length - 1
+  } else {
+    counter--
+  }
+  updateSlidePosition()
+}
+
+function updateSlidePosition() {
   carouselSlide.style.transform = `translateX(-${counter * slideWidth}px)`
 }
 
@@ -27,4 +35,10 @@ document.addEventListener('keydown', (e) => {
   if (e.key === 'ArrowRight') nextSlide()
 })
 
+// Update slide width on window resize
+window.addEventListener('resize', updateSlideWidth)
+
+// Initial setup
+updateSlidePosition()
+updateSlideWidth()
 setInterval(nextSlide, 3000)
